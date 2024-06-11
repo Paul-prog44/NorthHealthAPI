8522476275dabb4f103c26589e56b6dfb0adda48
Paul-prog44 <saintouenfr@hotmail.com>
2024-06-11 11:29:47 +0200

    Add fct to compare email address

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -2,20 +2,42 @@
 
 namespace App\Controller;
 
+use mysqli;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
+
+//Controlleur pour vérifier l'authentification de l'utilisateur de l'appli mobile qui renvoie l'objet user
 class AuthenticationController extends AbstractController
 {
-    #[Route('/authentication', name: 'app_authentication', methods:['POST'])]
-    public function authentication(Request $request, SerializerInterface $serializer): Response
+    #[Route('/authentication', name: 'app_authentication', methods:['GET'])]
+    public function authentication(Request $request): Response
     {
         $jsonRequest = $request->getContent();
-        $dataArray = json_decode($jsonRequest, true);
-        dd($dataArray['nom']);
+        $credentials = json_decode($jsonRequest, true);
+
+        $mysqli = new mysqli("127.0.0.1", "root", "", "north_health", 3306);
+        // Vérification de la connexion
+        if ($mysqli->connect_error) {
+            die("Connection failed: " . $mysqli->connect_error);
+        }
+
+        $emailAddress = $mysqli->real_escape_string($credentials['emailAddress']);
+        $sql = "SELECT * FROM patient WHERE email_address = '$emailAddress'";
+
+        //Construction de la requête
+
+        $result = $mysqli->query($sql);
+
+        if ($result) {
+            $allRows = $result->fetch_array(MYSQLI_ASSOC);
+            dd($allRows["address"]);
+            //Vérification du mot de passe
+        }
+
+        
 
         return $this->render('authentication/index.html.twig', [
             'controller_name' => 'AuthenticationController',
