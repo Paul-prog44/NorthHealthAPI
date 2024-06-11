f9fd108165d7d343883159f6cb2d13724d158e37
Paul-prog44 <saintouenfr@hotmail.com>
2024-06-11 17:14:47 +0200

    Change method on auth controller, fix denied ressources upon wrong credentials

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -17,10 +17,12 @@ use Symfony\Component\Serializer\SerializerInterface;
 //Controlleur pour vérifier l'authentification de l'utilisateur de l'appli mobile qui renvoie l'objet patient
 class AuthenticationController extends AbstractController
 {
-    #[Route('/authentication', name: 'app_authentication', methods:['GET'])]
-    public function authentication(Request $request, PatientRepository $patientRepository, SerializerInterface $serializer): Response
+    #[Route('/authentication', name: 'app_authentication', methods:['POST'])]
+    public function authentication(Request $request, PatientRepository $patientRepository, SerializerInterface $serializer): JsonResponse
     {
         $jsonRequest = $request->getContent();
+
+        //Transforme le json en tableau associatif
         $credentials = json_decode($jsonRequest, true);
         
 
@@ -34,27 +36,22 @@ class AuthenticationController extends AbstractController
         $emailAddress = $mysqli->real_escape_string($credentials['emailAddress']);
         $sql = "SELECT * FROM patient WHERE email_address = '$emailAddress'";
 
-
         $result = $mysqli->query($sql);
-
-        if ($result) {
-            $allRows = $result->fetch_array(MYSQLI_ASSOC);
+        $allRows = $result->fetch_array(MYSQLI_ASSOC);
+        
+        if ($allRows) {
             //Vérification du mot de passe
             if (password_verify( $credentials["password"], $allRows["password"])) {
                 $id = $allRows["id"];
-                $jsonPatient = $patientRepository->find($id);
+                $patient = $patientRepository->find($id);
 
-                $jsonPatient = $serializer->serialize($jsonPatient, 'json', ['groups' => 'getPatients']);
+                $jsonPatient = $serializer->serialize($patient, 'json', ['groups' => 'getPatients']);
                 return new JsonResponse($jsonPatient, Response::HTTP_OK, [], true);
             } else {
-                return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+                return new JsonResponse(null, Response::HTTP_UNAUTHORIZED);
             }
         }
 
-        
-
-        return $this->render('authentication/index.html.twig', [
-            'controller_name' => 'AuthenticationController',
-        ]);
+        return new JsonResponse(null, Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
